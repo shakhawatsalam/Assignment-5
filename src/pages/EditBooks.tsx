57fe1b5ba5ca0763392001eac1736d22f8885a8e
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-07-16 22:06:05 +0600

    [Handeling Edit Book From]

--- src/pages/EditBooks.tsx
+++ src/pages/EditBooks.tsx
@@ -1,32 +1,52 @@
-
+/* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useParams } from "react-router-dom";
 import NavBar from "../component/NavBar";
 import { useGetSingleBookQuery } from "../redux/api/apiSlice";
+import { useForm } from "react-hook-form";
+import { Book } from "../Types/globaltypes";
 
 export default function EditBooks() {
-   const { id } = useParams();
+  const { id } = useParams();
+  const { register, handleSubmit, reset } = useForm<Book>();
 
-   const { data } = useGetSingleBookQuery(id);
+  const { data } = useGetSingleBookQuery(id);
+  const book = data?.data;
+  console.log(book);
 
-   const book = data?.data;
+  const onSubmit = (data: Book) => {
+    const EditedBook = {
+      title: data.title ? data.title : book.title,
+      author: data.author ? data.author : book.author,
+      genre: data.genre ? data.genre : book.genre,
+      publicationDate: data.publicationDate
+        ? data.publicationDate
+        : book.publicationDate,
+      image: data.image ? data.image : book.image,
+      details: data.details ? data.details : book.details,
+    };
+    console.log(EditedBook);
+
+  };
   return (
     <>
       <NavBar />
       <div className='max-w-7xl mx-auto'>
         <div className='border flex items-center justify-center h-[100vh] flex-col'>
           <h1 className='text-5xl mb-3'>Edit Book</h1>
-          <form className='w-[700px]'>
+          <form className='w-[700px] ' onSubmit={handleSubmit(onSubmit)}>
             <div className='flex justify-between'>
               <div className='form-control w-full max-w-xs'>
                 <label className='label'>
                   <span className='label-text'>Title</span>
                 </label>
                 <input
+                  id='title'
+                  {...register("title")}
                   type='text'
-                  placeholder='Type here'
+                  placeholder={book?.title}
                   className='input input-bordered w-full max-w-xs'
                 />
               </div>
@@ -35,8 +55,10 @@ export default function EditBooks() {
                   <span className='label-text'>Author</span>
                 </label>
                 <input
+                  id='author'
+                  {...register("author")}
                   type='text'
-                  placeholder='Type here'
+                  placeholder={book?.author}
                   className='input input-bordered w-full max-w-xs'
                 />
               </div>
@@ -47,8 +69,10 @@ export default function EditBooks() {
                   <span className='label-text'>Genre</span>
                 </label>
                 <input
+                  id='genre'
+                  {...register("genre")}
                   type='text'
-                  placeholder='Type here'
+                  placeholder={book?.genre}
                   className='input input-bordered w-full max-w-xs'
                 />
               </div>
@@ -57,8 +81,10 @@ export default function EditBooks() {
                   <span className='label-text'>Publication Date</span>
                 </label>
                 <input
+                  id='publicationDate'
+                  {...register("publicationDate")}
                   type='text'
-                  placeholder='Type here'
+                  placeholder={book?.publicationDate}
                   className='input input-bordered w-full max-w-xs'
                 />
               </div>
@@ -69,8 +95,10 @@ export default function EditBooks() {
                   <span className='label-text'>Image</span>
                 </label>
                 <input
+                  id='image'
+                  {...register("image")}
                   type='text'
-                  placeholder='Type here'
+                  placeholder='Image Link'
                   className='input input-bordered w-full max-w-xs'
                 />
               </div>
@@ -79,15 +107,17 @@ export default function EditBooks() {
                   <span className='label-text'>Details</span>
                 </label>
                 <input
+                  id='details'
+                  {...register("details")}
                   type='text'
-                  placeholder='Type here'
+                  placeholder='Type Details here'
                   className='input input-bordered w-full max-w-xs'
                 />
               </div>
             </div>
             <div className='flex justify-center mt-5'>
               <button type='submit' className='btn btn-info'>
-                Add Book
+                Edit Book
               </button>
             </div>
           </form>
