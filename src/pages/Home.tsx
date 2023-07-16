9325e6d3e718e353c1f7241f8f5b4b4b822d5556
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-07-16 07:52:15 +0600

    [Card Design is Done]

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import BookCard from "../component/BookCard";
 import Footer from "../component/Footer";
 import HeroSection from "../component/HeroSection";
 import NavBar from "../component/NavBar";
@@ -6,12 +7,14 @@ export default function Home() {
   return (
     <>
       <div>
-        <NavBar/>
+        <NavBar />
         <HeroSection />
-        <Footer/>
-        <div className='max-w-7xl mx-auto'>
-
-        </div>
+        <h1 className='block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl lg:text-6xl dark:text-white text-center mt-14'>
+          Leatest Books
+        </h1>
+        <BookCard/>
+        <Footer />
+        <div className='max-w-7xl mx-auto'></div>
       </div>
     </>
   );
